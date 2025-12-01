4fc0c01ed8e4bb266b2fcf7aa743bb22b6c69dc3
PappG <PappG@310-14>
2025-12-01 08:32:29 +0100

    Menüpontok

--- zenelejatszas/Program.cs
+++ zenelejatszas/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            
+            while (true) 
+            {
+                Console.WriteLine("Keresés cím szerint");
+                Console.WriteLine("Zene törlése");
+                Console.WriteLine("Zene módosíter");
+                Console.WriteLine("Új zene");
+                Console.WriteLine("3.5 percnél hosszabbak");
+                Console.WriteLine("Csap PoP zenék");
+                Console.WriteLine("Zenék betöltése");
+
+                string valasztas = Console.ReadLine();
+                switch (valasztas) 
+                {
+                    case "0": return;
+                        case "1": 
+
+                            break;
+                        case "2":
+
+
+                            break;
+                        case "3":
+
+
+                            break;
+                        case "4":
+
+
+                            break;
+                        case "5":
+
+
+                        break;
+                }
+            }
         }
     }
 }
