49846ba9bf4547c5cd36caf2f3e653e0e8fa2644
PappG <PappG@310-14>
2025-12-01 10:12:35 +0100

    menu

--- zenelejatszas/Program.cs
+++ zenelejatszas/Program.cs
@@ -1,44 +1,81 @@
 ﻿namespace zenelejatszas
 {
+    class Zene
+    {
+        public string Cim;
+        public string Eloado;
+        public string Mufaj;
+        public int HosszMp;
+
+        public Zene(string cim, string eloado, string mufaj, int hosszMp)
+        {
+            Cim = cim;
+            Eloado = eloado;
+            Mufaj = mufaj;
+            HosszMp = hosszMp;
+        }
+
+        public override string ToString()
+        {
+            return $"{Cim} - {Eloado} ({Mufaj}, {HosszMp} mp)";
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            while (true) 
+           
+            // az első sor a fejléc → átugorjuk
+            
+            while (true)
             {
-                Console.WriteLine("Keresés cím szerint");
-                Console.WriteLine("Zene törlése");
-                Console.WriteLine("Zene módosíter");
-                Console.WriteLine("Új zene");
-                Console.WriteLine("3.5 percnél hosszabbak");
-                Console.WriteLine("Csap PoP zenék");
-                Console.WriteLine("Zenék betöltése");
-
+                Console.WriteLine("0. Kilépés");
+                Console.WriteLine("1. Keresés cím szerint");
+                Console.WriteLine("2. Zene törlése");
+                Console.WriteLine("3. Zene módosítása");
+                Console.WriteLine("4. Új zene");
+                Console.WriteLine("5. 3,5 percnél hosszabbak");
+                Console.WriteLine("6. Csak Pop zenék");
+                Console.WriteLine("7. Zenék betöltése");
                 string valasztas = Console.ReadLine();
-                switch (valasztas) 
+                switch (valasztas)
                 {
                     case "0": return;
-                        case "1": 
-
-                            break;
-                        case "2":
-
-
-                            break;
-                        case "3":
-
-
-                            break;
-                        case "4":
-
-
-                            break;
-                        case "5":
-
+                    case "1":
+                       
+                       
 
                         break;
+                    case "2":
+                       
+                        
+                        break;
+                    case "3":
+
+                        break;
+                    case "4":
+                         break;
+                    case "5":
+                        
+                        break;
+                    case "6":
+                        var popZenek = zenek
+        
+
+                       
+                        break;
+                    case "7":
+                        
+                        }
+                        break;
+
+
                 }
+                Console.ReadKey();
+
             }
         }
     }
 }
+
+    
